f0b4e29a11a6f1268e104017110e6d3c67cbaf15
Alejandro J. Rojas <ale@ischool.berkeley.edu>
2015-10-17 22:25:25 -0500

    hadoop

--- exercise_1/loading_and_modeling/load_data_lake.sh
+++ exercise_1/loading_and_modeling/load_data_lake.sh
@@ -3,15 +3,15 @@
 #### https://data.medicare.gov/views/bg9k-emty/files/Ma46xU4I05xsIKuEqLLi-N-s7GoO2ZefzJ7SYyTIKjA?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip
 
 
-#### Let's create directories
 
 
 
-cd /root/raw_data
 
-wget https://data.medicare.gov/views/bg9k-emty/files/Ma46xU4I05xsIKuEqLLi-N-s7GoO2ZefzJ7SYyTIKjA?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip
 
-unzip "Ma46xU4I05xsIKuEqLLi-N-s7GoO2ZefzJ7SYyTIKjA?content_type=application%2Fzip; charset=binary"
+wget https://data.medicare.gov/views/bg9k-emty/files/Ma46xU4I05xsIKuEqLLi-N-s7GoO2ZefzJ7SYyTIKjA?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip /root/raw_data
+
+unzip /root/raw_data/"Ma46xU4I05xsIKuEqLLi-N-s7GoO2ZefzJ7SYyTIKjA?content_type=application%2Fzip; charset=binary"
+
 
 
 tail -n +2 /root/raw_data/"Hospital General Information.csv" > /root/user/ale/hospital_compare/hospital.csv
@@ -23,4 +23,12 @@ tail -n +2 /root/raw_data/"Readmissions and Deaths - Hospital.csv" > /root/user/
 tail -n +2 /root/raw_data/hvbp_tps_08_06_2015.csv > /root/user/ale/hospital_compare/surveys_responses.csv
 
 
+hadoop fs -mkdir /root/hdfs/user/ale/hospital_compare
 
+hadood fs -put /root/user/ale/hospital_compare/hospital.csv /root/hdfs/user/ale/hospital_compare
+
+hadood fs -put /root/user/ale/hospital_compare/effective_care.csv /root/hdfs/user/ale/hospital_compare
+
+hadood fs -put /root/user/ale/hospital_compare/readmissions.csv /root/hdfs/user/ale/hospital_compare
+
+hadood fs -put /root/user/ale/hospital_compare/surveys_responses.csv /root/hdfs/user/ale/hospital_compare
