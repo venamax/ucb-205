d27b1e9c905e440608a5cbc2d014481a5e0a6ff8
Alejandro J. Rojas <ale@ischool.berkeley.edu>
2015-10-18 23:31:12 -0500

    Merge branch 'master' into 205-labs
    Conflicts:
    	exercise_1/loading_and_modeling/load_data_lake.sh

--- exercise_1/loading_and_modeling/load_data_lake.sh
+++ exercise_1/loading_and_modeling/load_data_lake.sh
@@ -7,9 +7,11 @@
 
 
 
+
 wget https://data.medicare.gov/views/bg9k-emty/files/Ma46xU4I05xsIKuEqLLi-N-s7GoO2ZefzJ7SYyTIKjA?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip 
 
 unzip "Ma46xU4I05xsIKuEqLLi-N-s7GoO2ZefzJ7SYyTIKjA?content_type=application%2Fzip; charset=binary"
+
 
 
 
