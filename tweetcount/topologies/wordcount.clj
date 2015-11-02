ad05409cd0c01022ff6d4580e926fbb9149ce5cd
Alejandro J. Rojas <ale@ischool.berkeley.edu>
2015-11-02 16:45:37 -0600

    lab9 changes

--- tweetcount/topologies/wordcount.clj
+++ tweetcount/topologies/wordcount.clj
@@ -2,20 +2,29 @@
   (:use     [streamparse.specs])
   (:gen-class))
 
-(defn wordcount [options]
+(defn tweetcount [options]
    [
     ;; spout configuration
-    {"word-spout" (python-spout-spec
+    {"sentence-spout" (python-spout-spec
           options
-          "spouts.words.WordSpout"
-          ["word"]
+          "spouts.sentences.Sentences"
+          ["sentence"]
           )
     }
-    ;; bolt configuration
+    ;; bolt configuration 1
+    {"parse-bolt" (python-bolt-spec
+          options
+          {"sentence-spout" :shuffle}
+          "bolts.parse.ParseTweet"
+          ["valid_word"]
+          :p 2
+          )
+    }
+    ;; bolt configuration 2
     {"count-bolt" (python-bolt-spec
           options
-          {"word-spout" :shuffle}
-          "bolts.wordcount.WordCounter"
+          {"parse-bolt" :shuffle}
+          "bolts.tweetcounter.TweetCounter"
           ["word" "count"]
           :p 2
           )
